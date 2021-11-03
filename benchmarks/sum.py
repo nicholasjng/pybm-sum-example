19eb0490e6a6da80d3765351e282046f39f44d6f
Nicholas Junge <nicholas.junge@web.de>
2021-11-03 15:08:32 +0100

    Remove sys.path hack, running benchmarks in module mode instead

--- benchmarks/sum.py
+++ benchmarks/sum.py
@@ -1,16 +1,10 @@
-import os
-import sys
-
 import pybm
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from main import my_sum
 
 
 def f():
-    return my_sum(100000)
+    return my_sum(10000)
 
 
 if __name__ == "__main__":
